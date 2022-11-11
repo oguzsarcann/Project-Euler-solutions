25b4b2f97193cd905e4fab09562d070760a80598
oğuz sarcan <oguz.sarcan@icloud.com>
2022-11-11 16:08:08 +0300

    Project-Euler-solutions

---
+++ problem6.py
@@ -0,0 +1,40 @@
+"""The sum of the squares of the first ten natural numbers is,
+                1^2 + 2^2 + 3^2 + ...... + 10^2 =385
+
+The square of the sum of the first ten natural numbers is,
+
+                (1+2+3+.......+10)^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+3025-385=2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+"""
+
+def sumofsquares(x):
+    sum = 0
+    for i in range(1,x+1):
+        sum += i**2
+    return sum
+def squareofsum(x):
+    sum = 0
+    for i in range(1,x+1):
+        sum += i
+    return(sum**2)
+def differenceofthesums(x):
+    return squareofsum(x) - sumofsquares(x)
+
+print(differenceofthesums(100))
+
+"""
+we also know that
+the sum of the first n natural numbers= n*(n+1)/2. So squareofsum(n) = (n*(n+1)/2)**2
+the sum of the squares of the first n natural numbers:=sumofsquares(n)=n*(n+1)*(2*n+1)/6
+
+def differenceofsums(n):
+    return int(((n*(n+1)/2)**2) - (n*(n+1)*(2*n+1)/6))
+
+print(differenceofsums(100))
+
+"""
